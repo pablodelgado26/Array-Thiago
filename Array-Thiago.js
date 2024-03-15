f39e8cc3bf86758a2405a70ef51a62576148e5e3
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-03-15 18:33:30 -0300

    .Contact

---
+++ Array-Thiago.js
@@ -0,0 +1,6 @@
+// .Contact, uni valores ou Arrays e retorna uma nova Array com todos valores com a ordem passada.
+let a = [1, 2, 3,];
+let b = [4, 5, 6,];
+
+let sequencia = a.concat(b);
+console.log(sequencia);
